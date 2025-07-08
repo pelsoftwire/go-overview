4f8311d89b4fd59dac9f36b767ca546ccc4a8d10
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-08 11:01:11 +0100

    added thanks for creators and name length check and truncating name after space (sorry large commit)

--- main.go
+++ main.go
@@ -4,7 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+func isCreator(e string) bool {
+	var creators = [3]string{"Robert Griesemer", "Rob Pike", "Ken Thompson"}
+	for _, a := range creators {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
 
 func getName() string {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -19,5 +30,24 @@ func main() {
 }
 
 func greeting(name string) string {
-	return "Hello, " + name
+	// length limit 20
+	formattedName := name
+
+	if strings.Index(formattedName, " ") != -1 {
+		formattedName = formattedName[:strings.Index(formattedName, " ")]
+	}
+	if len(formattedName) > 20 {
+		formattedName = formattedName[:21]
+	}
+
+	// note: formatted name is used to decide on greeting addons
+	sentence := "Hello, " + formattedName
+	if isCreator(name) {
+		sentence = sentence + " Thanks for creating me!"
+	}
+	if len(name) > 20 {
+		sentence = sentence + "... Wow, that name's too long for me!"
+	}
+
+	return sentence
 }
--- main_test.go
+++ main_test.go
@@ -8,6 +8,11 @@ var tests = []struct {
 }{
 	{"Alice", "Hello, Alice"},
 	{"Bob", "Hello, Bob"},
+	{"Robert Griesemer", "Hello, Robert Thanks for creating me!"},
+	{"Rob Pike", "Hello, Rob Thanks for creating me!"},
+	{"Ken Thompson", "Hello, Ken Thanks for creating me!"},
+	{"Pel 123456789123456789", "Hello, Pel... Wow, that name's too long for me!"},
+	{"Pel B", "Hello, Pel"},
 }
 
 func TestGreeting(t *testing.T) {
